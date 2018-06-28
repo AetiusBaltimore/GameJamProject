fbeff4ca2f40357194784072b40150d6040795d9
Aetius <AetiusBaltimore@gmail.com>
2018-06-27 21:51:36 -0400

    Shooting, Camera, Enemy
    General overhaul of everything.
    - Shooting revamped
    - enemy health system
    - player health system
    - pause menu
    - player death
    - enemy death
    - moddable dash duration, speed, and cd.
    - bullet cd.

---
+++ Assets/Scripts/Player/HUDupdate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class HUDupdate : MonoBehaviour {
+
+	PlayerStats ps;
+	
+	public float healthValue;
+	public float energyValue;
+	
+	float healthBarFill;
+	float energyBarFill;
+	
+	public float healthBarMax;
+	public float energyBarMax;
+	
+	public Image healthBar;
+	public Image energyBar; 
+	
+	
+	void Start(){
+		ps = GameObject.FindWithTag("Player").GetComponent<PlayerStats>();
+	
+		healthBar.fillAmount = 1f;
+		energyBar.fillAmount = 1f;
+	}
+	
+	void Update(){
+		UpdateHPBar();
+		UpdateEBar(); 
+	}
+	
+	void UpdateHPBar(){
+		healthValue = (ps.Health/healthBarMax);
+		healthBar.fillAmount = healthValue; 
+	}
+	
+	void UpdateEBar(){
+		energyValue = (ps.Energy/energyBarMax);
+		energyBar.fillAmount = energyValue; 
+	}
+}
--- Assets/Scripts/Player/PlayerAttacks.cs
+++ Assets/Scripts/Player/PlayerAttacks.cs
@@ -4,13 +4,74 @@ using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public GameObject projectilePrefab; 
+	
+	PlayerMovement PM; 
 		
+	GameObject curBullet;
+	Vector2 curBulletDir;
+	Quaternion curBulletRot; 
+	
+	float projectileVelocity; 
+	float aimY; 
+
+	void Start () {
+		PM = GetComponent<PlayerMovement>(); 
+		
+		projectileVelocity = 50000f; 
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown("Shoot Bullet")){
+			StartCoroutine("ShootBullet"); 
+		}
+	}
+	
+	void CreateBullet(){
+		curBullet = (GameObject)Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+	}
+	
+	IEnumerator ShootBullet(){
+		CreateBullet();
+		CreateBulletDir();
+		MoveBullet();
+		yield return new WaitForSeconds(.2f); 
+	}
+	
+	void MoveBullet(){
+		Rigidbody2D bulletRB; 
+			bulletRB = curBullet.GetComponent<Rigidbody2D>(); 
+			curBullet.transform.rotation = curBulletRot;
+			if (curBullet != null){ 
+				bulletRB.AddForce(curBulletDir*Time.deltaTime*projectileVelocity); 
+			}
+	}
+	
+	void CreateBulletDir(){
+		aimY = Input.GetAxis("Vertical");
+		if(PM.FacingLeft){
+			
+			if (aimY >= .1f){
+				curBulletDir = new Vector2(-1f,1f);
+				curBulletRot = Quaternion.AngleAxis(-45f, Vector3.forward); 
+			} else if (aimY <= -.1f){
+				curBulletDir = new Vector2(-1f,-1f);
+				curBulletRot = Quaternion.AngleAxis(45f, Vector3.forward);
+			} else {
+				curBulletDir = Vector2.left;
+				curBulletRot = Quaternion.AngleAxis(0f, Vector3.forward);
+			}
+		} else { 
+			if (aimY >= .1f){
+				curBulletDir = new Vector2(1f, 1f);
+				curBulletRot = Quaternion.AngleAxis(45f, Vector3.forward);
+			} else if (aimY <= -.1f){
+				curBulletDir = new Vector2(1f,-1f);
+				curBulletRot = Quaternion.AngleAxis(-45f, Vector3.forward);
+			} else {
+				curBulletDir = Vector2.right;
+				curBulletRot = Quaternion.AngleAxis(0f, Vector3.forward);
+			}
+		}
 	}
 }
