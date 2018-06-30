1c9c33173b0986b7318c98e719801ed2bcc0c959
Aetius <AetiusBaltimore@gmail.com>
2018-06-30 18:17:30 -0400

    Test Scene changes

---
+++ Assets/Scripts/Bosses/Square/SquareAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareAI : MonoBehaviour {
+
+	bool updateState; 
+
+	public enum AIState{
+		
+	}
+	
+	void Start(){
+		updateState = true; 
+	}
+	
+	void Update(){
+		if (updateState){
+			StartCoroutine("MakeDecision"); 
+			
+		}
+	}
+}
